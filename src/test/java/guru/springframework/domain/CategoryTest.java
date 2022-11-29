f65592d2a04707c117b6e396241ed73438fc2440
BASSET, Thomas <thomas.basset@cgi.com>
2022-11-29 08:38:19 +0100

    Converting JUnit 4 Tests to JUnit 5

--- src/test/java/guru/springframework/domain/CategoryTest.java
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -1,7 +1,7 @@
 package guru.springframework.domain;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -9,7 +9,7 @@ public class CategoryTest {
 
     Category category;
 
-    @Before
+    @BeforeEach
     public void setUp(){
         category = new Category();
     }
